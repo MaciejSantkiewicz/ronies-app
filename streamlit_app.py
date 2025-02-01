7576aed7c18caff838686d4b2c0e510f5b6276c4
Maciej <maciej.santkiewicz@gmail.com>
2025-02-01 15:01:47 +0100

    link

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,17 @@ st.title("Kur3w2323a1")
 st.write(
     "Let's start building! For help and inspir."
 )
+
+
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
