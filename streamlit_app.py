25a3e32f9fc514b7b1704e591cda4bcead5751a6
Maciej <maciej.santkiewicz@gmail.com>
2025-02-01 14:56:33 +0100

    11111

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Kurwa1")
+st.title("Kurw2323a1")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
